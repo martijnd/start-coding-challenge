17996b3beba7724fd99b4f5921257a97b5fff8e6
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-08-06 18:50:18 +0200

    Add contents

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+#!/usr/bin/env node
+const main = async () => {
+  console.log('hello');
 
+  process.exit(0);
+};
+
+main().catch((err) => {
+  console.error("Aborting installation...");
+  if (err instanceof Error) {
+    console.error(err);
+  } else {
+    console.error(
+      "An unknown error has occurred. Please open an issue on github with the below:",
+    );
+    console.log(err);
+  }
+  process.exit(1);
+});
